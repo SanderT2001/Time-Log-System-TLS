6851fa0dfd1caa0fae8fb2d47b82ab16a9e8c5a7
Justin <justinrijsdijk@gmail.com>
2019-06-11 13:49:27 +0200

    Update time_log_system_tls.sql

--- SQL/time_log_system_tls.sql
+++ SQL/time_log_system_tls.sql
@@ -2,10 +2,10 @@
 -- version 4.8.4
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: May 23, 2019 at 03:36 PM
--- Server version: 10.1.37-MariaDB
--- PHP Version: 7.3.0
+-- Host: 127.0.0.1:3306
+-- Generation Time: Jun 11, 2019 at 11:48 AM
+-- Server version: 5.7.23
+-- PHP Version: 7.2.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `time_log_system_tls_production`
+-- Database: `tlsv2`
 --
 
 -- --------------------------------------------------------
@@ -28,8 +28,9 @@ SET time_zone = "+00:00";
 -- Table structure for table `clients`
 --
 
-CREATE TABLE `clients` (
-  `ID` int(11) NOT NULL,
+DROP TABLE IF EXISTS `clients`;
+CREATE TABLE IF NOT EXISTS `clients` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
   `client_first_name` varchar(35) NOT NULL,
   `client_middle_name` varchar(35) DEFAULT NULL,
   `client_last_name` varchar(35) NOT NULL,
@@ -42,7 +43,9 @@ CREATE TABLE `clients` (
   `client_postal_code` varchar(35) DEFAULT NULL,
   `client_country` varchar(100) DEFAULT NULL,
   `client_description` text,
-  `by_user` int(11) NOT NULL
+  `by_user` int(11) NOT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `client_postal_code` (`client_postal_code`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -51,8 +54,9 @@ CREATE TABLE `clients` (
 -- Table structure for table `logs`
 --
 
-CREATE TABLE `logs` (
-  `ID` int(11) NOT NULL,
+DROP TABLE IF EXISTS `logs`;
+CREATE TABLE IF NOT EXISTS `logs` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
   `time_type_id` int(11) NOT NULL,
   `project_id` int(11) DEFAULT '0',
   `log_summary` varchar(255) NOT NULL,
@@ -62,7 +66,8 @@ CREATE TABLE `logs` (
   `log_start_time` time NOT NULL,
   `log_end_time` time NOT NULL,
   `log_time_diff_min` int(11) NOT NULL,
-  `by_user` int(11) NOT NULL
+  `by_user` int(11) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -71,8 +76,9 @@ CREATE TABLE `logs` (
 -- Table structure for table `projects`
 --
 
-CREATE TABLE `projects` (
-  `ID` int(11) NOT NULL,
+DROP TABLE IF EXISTS `projects`;
+CREATE TABLE IF NOT EXISTS `projects` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
   `project_name` varchar(255) NOT NULL,
   `project_description` text NOT NULL,
   `project_retrospective` text,
@@ -80,7 +86,8 @@ CREATE TABLE `projects` (
   `project_progress` int(3) DEFAULT NULL,
   `project_start_date` date DEFAULT NULL,
   `project_end_date` date DEFAULT NULL,
-  `by_user` int(11) NOT NULL
+  `by_user` int(11) NOT NULL,
+  PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -89,12 +96,14 @@ CREATE TABLE `projects` (
 -- Table structure for table `time_types`
 --
 
-CREATE TABLE `time_types` (
-  `ID` int(11) NOT NULL,
+DROP TABLE IF EXISTS `time_types`;
+CREATE TABLE IF NOT EXISTS `time_types` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
   `type_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `type_description` text COLLATE utf8_unicode_ci NOT NULL,
   `type_global_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `by_user` int(11) NOT NULL
+  `by_user` int(11) NOT NULL,
+  PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
@@ -103,8 +112,9 @@ CREATE TABLE `time_types` (
 -- Table structure for table `users`
 --
 
-CREATE TABLE `users` (
-  `ID` int(11) NOT NULL,
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
   `first_name` varchar(35) NOT NULL,
   `middle_name` varchar(35) DEFAULT NULL,
   `last_name` varchar(35) NOT NULL,
@@ -112,77 +122,9 @@ CREATE TABLE `users` (
   `password` varchar(255) NOT NULL,
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL,
-  `last_login_at` datetime DEFAULT NULL
+  `last_login_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `clients`
---
-ALTER TABLE `clients`
-  ADD PRIMARY KEY (`ID`),
-  ADD KEY `client_postal_code` (`client_postal_code`);
-
---
--- Indexes for table `logs`
---
-ALTER TABLE `logs`
-  ADD PRIMARY KEY (`ID`);
-
---
--- Indexes for table `projects`
---
-ALTER TABLE `projects`
-  ADD PRIMARY KEY (`ID`);
-
---
--- Indexes for table `time_types`
---
-ALTER TABLE `time_types`
-  ADD PRIMARY KEY (`ID`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`ID`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `clients`
---
-ALTER TABLE `clients`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `logs`
---
-ALTER TABLE `logs`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `projects`
---
-ALTER TABLE `projects`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `time_types`
---
-ALTER TABLE `time_types`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
